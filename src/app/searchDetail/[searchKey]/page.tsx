a9714b9ca8e6eaba559977b9e6c5f15217999178
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2025-02-27 23:58:00 +0300

    edited searchDetail page

--- src/app/searchDetail/[searchKey]/page.tsx
+++ src/app/searchDetail/[searchKey]/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { API_URL, API_KEY } from "../../../../config";
+import { useParams } from "next/navigation";
 
 const SearchDetailPage = () => {
-  return <div>SearchDetail</div>;
+  const params = useParams();
+  const searchKey = params?.searchKey as string;
+  const [newsBySearchKey, setNewsBySearchKey] = useState([]);
+  console.log(newsBySearchKey);
+  useEffect(() => {
+    const fetchNewsBySearchKey = async () => {
+      const response = await fetch(
+        `https://newsapi.org/v2/everything?q=${searchKey}&apiKey=${API_KEY}`
+      );
+      const data = await response.json();
+      setNewsBySearchKey(data.articles);
+    };
+    fetchNewsBySearchKey();
+  }, []);
+
+  return <div>safsaf</div>;
 };
 
 export default SearchDetailPage;
