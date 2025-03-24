6170cda290dae8f0660c98221be5b9db29a37f21
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2025-03-24 23:49:28 +0300

    solved api error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,35 +6,56 @@ import { API_URL, API_KEY } from "../../config";
 
 export default function Home() {
   const [newsData, setNewsData] = useState<NewsCardProps[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch(
-        `${API_URL}?sources=bbc-news&apiKey=${API_KEY}`
-      );
-      const data = await response.json();
-      setNewsData(data.articles || []);
+      setLoading(true);
+      setError(null);
+
+      try {
+        const response = await fetch(
+          `${API_URL}?sources=bbc-news&apiKey=${API_KEY}`
+        );
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        setNewsData(data.articles || []);
+      } catch (err: any) {
+        console.error("Error fetching data:", err);
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
     };
+
     fetchData();
   }, []);
 
   return (
     <div className="pt-16 min-h-screen">
+      {loading && <p>Loading news...</p>}
+
+      {error && <p>An error occured while receiving data: {error}</p>}
+
       <ul className="grid-container">
-        {newsData && Array.isArray(newsData) ? (
-          newsData.map((item, index) => (
-            <li key={index}>
-              <HomePageCard
-                author={item.author}
-                description={item.description}
-                title={item.title}
-                urlToImage={item.urlToImage}
-                url={item.url}
-              />
-            </li>
-          ))
-        ) : (
-          <p>Haberler yükleniyor veya veri yok...</p>
-        )}
+        {newsData.length > 0
+          ? newsData.map((item, index) => (
+              <li key={index}>
+                <HomePageCard
+                  author={item.author}
+                  description={item.description}
+                  title={item.title}
+                  urlToImage={item.urlToImage}
+                  url={item.url}
+                />
+              </li>
+            ))
+          : !loading && !error && <p>No news was found.</p>}
       </ul>
     </div>
   );
