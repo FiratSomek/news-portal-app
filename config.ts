37aec0dfb246d79e3d4a124ddeaafb5b6fe6bdbb
FiratSomek <121132276+FiratSomek@users.noreply.github.com>
2025-03-26 00:34:40 +0300

    ..

--- config.ts
+++ config.ts
@@ -1,2 +1,2 @@
-export const API_URL = "https://newsapi.org/v2/top-headlines/";
+export const API_URL = "https://newsapi.org/v2/top-headlines";
 export const API_KEY = "83e0379495354d1cadf88393462a190a";
